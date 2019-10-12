0a892412c4d325a49725e9c1dd3b650f0e0fabaf
Himanshu Khudania <hkhudania@gmail.com>
2019-10-12 22:19:29 +0530

    Redirecting Request

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 // function rqListner(req, res) {
 //     console.log('Hello')
@@ -14,12 +15,19 @@ const server = http.createServer((req, res) => {
     // console.log(req.url, req.method, req.headers);
 
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+fs.writeFileSync('message.txt', 'DUMMY');
+res.statusCode = 302;
+res.setHeader('Location', '/');
+return res.end();
     }
 
     res.setHeader('Content-Type', 'text/html');

9bdf141c83160ae2853a76bce6f4e96c0b7b0ef1
Himanshu Khudania <hkhudania@gmail.com>
2019-10-13 14:41:32 +0530

    Started with express and added Middleware

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+const http = require('http')
+const express = require('express')
+
+const app = express()
+
+app.use((req, res, next) => {
+    console.log('In middleware')
+    next() //Allows the request to continue to the next middleware in line
+})
+
+app.use((req, res, next) => {
+    console.log('In 2 middleware')
+
+})
+
+const server = http.createServer(app)
+server.listen(3000)
+
+
+
+
 // const http = require('http');
 
 // const routes = require('./routes');
@@ -28,12 +49,3 @@
 // server.listen(3000);
 
 
-const http = require('http');
-
-const routes = require('./routes');
-
-console.log(routes.someText);
-
-const server = http.createServer(routes.handler);
-
-server.listen(3000);

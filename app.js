f3e416a4e92e0db2516c513bda890c17800ea82c
Himanshu Khudania <hkhudania@gmail.com>
2019-10-13 15:21:07 +0530

    Added Body-parser

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 // const http = require('http')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log('In middleware')
-    next() //Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// app.use((req, res, next) => {
+//     console.log('In middleware')
+//     next() //Allows the request to continue to the next middleware in line
+// })
+
+// app.use('/', (req, res, next) => {
+//     console.log('This always Runs!')
+//     next()
+// })
+
+app.use('/add-product', (req, res, next) => {
+    // console.log('In middleware')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>')
 })
 
-app.use((req, res, next) => {
-    console.log('In 2 middleware')
+app.use('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
+    // console.log('In middleware')
     res.send('<h1>Hello From Express</h1>')
 })
 

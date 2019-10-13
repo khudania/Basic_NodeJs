08c701e3f94f685e6401850be08df2b277f4b489
Himanshu Khudania <hkhudania@gmail.com>
2019-10-13 15:39:40 +0530

    Seperated the Routes Modules

--- app.js
+++ app.js
@@ -3,20 +3,16 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+const adminRoutes = require('./routes/admin')
+
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>')
-})
+app.use(adminRoutes)
+app.use(shopRoutes)
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/')
-})
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello From Express</h1>')
-})
 
 app.listen(3000);
 

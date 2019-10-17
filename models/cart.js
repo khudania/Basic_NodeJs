6545bf9ea772f05e0fbe7ca112f29e840113f3bd
Himanshu Khudania <hkhudania@gmail.com>
2019-10-17 15:19:50 +0530

    Delete Item

--- models/cart.js
+++ models/cart.js
@@ -37,4 +37,21 @@ module.exports = class Cart {
       });
     });
   }
+
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return;
+      }
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.findIndex(prod => prod.id === id)
+      const productQty = prodcut.qty;
+      updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+      updatedCart.totalPrice = cart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      });
+    });
+  }
 };
